f353337b7e0dea2dab8899b55fa8c01abe93ed7c
emanbadr <amonaelshafey252@gmail.com>
2025-04-13 09:37:52 +0200

    handle auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
@@ -11,7 +10,6 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
 
 @NgModule({
   declarations: [
-    AuthComponent,
     LoginComponent,
     RegisterComponent,
     ForgetPasswordComponent,
