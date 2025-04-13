251218668ba603335f65c7c57fd3cf5eb9c31345
emanbadr <amonaelshafey252@gmail.com>
2025-04-13 08:47:19 +0200

    arch-EB

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , redirectTo:'landing' , pathMatch:'full'},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
