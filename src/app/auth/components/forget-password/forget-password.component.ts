4da49b63a1a06693da08be96809add74c2ea5dc3
emanbadr <amonaelshafey252@gmail.com>
2025-04-14 05:08:22 +0200

    create auth service

--- src/app/auth/components/forget-password/forget-password.component.ts
+++ src/app/auth/components/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent {
+  forgetPassword:FormGroup= new FormGroup({
+    email :new  FormControl('' , [Validators.email , Validators.required ])
+  })
+  constructor(){}
+
+  onSubmit(form:any):void{
+    console.log('done');
+    console.log(form);
+  }
 
 }
