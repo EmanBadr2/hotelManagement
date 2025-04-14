085336941b364415ef41cadd244e9a19d621aef7
WalaaElqady <walaaatef194@gmail.com>
2025-04-14 11:31:47 +0200

    Save my changes before merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AuthInputsComponent } from './shared/components/auth-inputs/auth-inputs
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent],
