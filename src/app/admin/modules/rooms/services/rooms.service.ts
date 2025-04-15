eff9282c838b971b8510c56be998091a6967a211
emanbadr <amonaelshafey252@gmail.com>
2025-04-16 00:13:13 +0200

    handle interceptor & login

--- src/app/admin/modules/rooms/services/rooms.service.ts
+++ src/app/admin/modules/rooms/services/rooms.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  onGettingAllRooms( params :any ):Observable<any>{
+    return this._HttpClient.get(`admin/rooms` , {params : params})
+  }
+
 }
