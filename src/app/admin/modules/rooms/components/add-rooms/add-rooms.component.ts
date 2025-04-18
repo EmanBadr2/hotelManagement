1540de2fcf5efa5a94e847986bc21ede829274d8
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-04-18 21:20:47 +0200

    uplode

--- src/app/admin/modules/rooms/components/add-rooms/add-rooms.component.ts
+++ src/app/admin/modules/rooms/components/add-rooms/add-rooms.component.ts
@@ -3,9 +3,8 @@ import { RoomsService } from '../../services/rooms.service';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
 
 
 
@@ -15,29 +14,47 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./add-rooms.component.scss']
 })
 
-export class AddRoomsComponent implements OnInit {
+export class AddRoomsComponent  implements OnInit{
+
+  activeRoomID!:any ;
+  isEditMode : boolean = false;
+  isViewMode : boolean = false ;
+  isFormDisabled: any ;
 
   addRoomForm !: FormGroup
   files: File[] = [];
+
   constructor(private _RoomsService:RoomsService ,
     private fb:FormBuilder ,
     private _Router:Router ,
+    private _ActivatedRoute:ActivatedRoute ,
     private _ToastrService:ToastrService
-  ){}
+  ) { }
 
-  ngOnInit(): void {
-    this.addRoomForm = this.fb.group({
-      roomNumber: ['' , Validators.required ],
-      price: ['' , Validators.required ],
-      discount: ['' , Validators.required ],
-      capacity:['' , Validators.required ],
-      // facilities:['' , Validators.required ],
-      facilities: this.fb.array([this.fb.control('')])  , // FormArray of controls
-      images: [null]                   // Placeholder for image files
-    })
+ ngOnInit(): void {
+  this.activeRoomID =this._ActivatedRoute.snapshot.paramMap.get('id')
+  this.isFormDisabled = this._ActivatedRoute.snapshot.queryParamMap.get('isFormDisabled')
+  // console.log( this.activeRoomID , this.isFormDisabled);
+     if(this.activeRoomID){ //  pass Data to Form (View & Edit)
+     //  get rooms by id
+      this.isEditMode= true
+      this.isViewMode= true
+    if( this.isFormDisabled == 'true' || true){
+      // View Mode
 
-  }
+       this.isEditMode= false
+       this.addRoomForm.disable()
+      }
 
+   }
+   console.log('edit' , this.isEditMode);
+   console.log('view' , this.isViewMode);
+
+
+
+ }
+
+// -----------
    // Getter for easy access
    get facilities(): FormArray {
     return this.addRoomForm.get('facilities') as FormArray;
@@ -52,24 +69,24 @@ export class AddRoomsComponent implements OnInit {
   // Dropzone file event
   onSelect(event: any) {
     this.files.push(...event.addedFiles);
-    this.addRoomForm.patchValue({ images: this.files });
   }
-
   onRemove(event: File) {
-    this.files = this.files.filter(f => f !== event);
-    this.addRoomForm.patchValue({ images: this.files });
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
   }
 // ------------
 
-  onSubmit(form:FormGroup):void{
+  Submit(form:FormGroup):void{
     console.log(form.value);
     this.addRoomForm = form
+    let formValues = form.value
     const formData = new FormData();
 
+   // --------------------------
      // // Append all controls
-     Object.entries(this.addRoomForm.value).forEach(([key, value]) => {
-      formData.append(key, value as string);
-    });
+     for(const key in formValues){
+      if (formValues.hasOwnProperty(key)) {  formData.append(key, formValues[key]) }
+    }
     // Append array of inputs
     this.facilities.controls.forEach((control, index) => {
       formData.append(`facilities[${index}]`, control.value);
@@ -78,12 +95,23 @@ export class AddRoomsComponent implements OnInit {
     this.files.forEach((file, index) => {
       formData.append(`images`, file, file.name);
     });
+    // --------------------------
+
+    if(!this.activeRoomID ){
+      //call Add
+      this.addRoom(formData)
+    }else{
+      // call    Edit/Updata
+      this.onUpdateRoom(this.activeRoomID , formData)
+    }
+
+  }
 
 
+  addRoom(formData :any):void{
     this._RoomsService.onAddRoom(formData).subscribe({
       next: (res) => {
         console.log('done');
-
         console.log(res);
         this._ToastrService.success('Room Added successfully')
         this._Router.navigate(['/admin/rooms/rooms'])
@@ -100,27 +128,53 @@ export class AddRoomsComponent implements OnInit {
 
   }
 
+  viewRoom(id:number| string):void{
+    this._RoomsService.viewRoomDetails(id).subscribe({
+      next: (res) => {
+        console.log('view done');
+        console.log(res);
+        //path value to my form
+        this.addRoomForm.patchValue({
+          // roomNumber: res.data.rooms.roomNumber,
+          // price: res.,
+          // discount: res.  ,
+          // capacity:res.  ,
+          // // facilities:res.,
+          // facilities:
+          // images:
+
+         }
+       );
+
+      },
+      error: (err) => {
+        console.log(err);
+        this._ToastrService.error(' error in View Room')
+      },
+
+    })
 
 
 
+  }
+
+  onUpdateRoom(id:number| string , formData : any):void{
+    this._RoomsService.editRoom(id , formData).subscribe({
+      next: (res) => {
+        console.log(res);
+        this._ToastrService.success('Room Updated Successfully ')
+      },
+      error: (err) => {
+        console.log(err);
+        this._ToastrService.error(' error in edit Room')
+      },
+      complete:() =>{
+        this._Router.navigate(['/admin/rooms/rooms'])
+      },
+    })
+  }
 
 
-  // addRoomForm : FormGroup = new FormGroup({
-  //   roomNumber: new FormControl('' , [Validators.required ]),
-  //   price: new FormControl('' , [Validators.required ]),
-  //   discount: new FormControl('' , [Validators.required ]),
-  //   capacity:new FormControl('' , [Validators.required ]),
-  //   facilities:new FormControl('' , [Validators.required ]),
-  // })
 
-
-  // onSelect(event: { addedFiles: any; }) {
-  //   console.log(event);
-  //   this.files.push(...event.addedFiles);
-  // }
-  // onRemove(event: File) {
-  //   console.log(event);
-  //   this.files.splice(this.files.indexOf(event), 1);
-  // }
 
 }
--- src/app/admin/modules/rooms/services/rooms.service.ts
+++ src/app/admin/modules/rooms/services/rooms.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IRoomsRes } from '../interfaces/IRooms';
-import { IAddRoomRes } from '../interfaces/IAddRooms';
 
 
 
@@ -17,20 +16,20 @@ export class RoomsService {
     return this._HttpClient.get<IRoomsRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms` , {params : params})
   }
 
-  onAddRoom(data:FormData):Observable<IAddRoomRes>{
-    return this._HttpClient.post<IAddRoomRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms` , data)
+  onAddRoom(data:FormData):Observable<IRoomsRes>{
+    return this._HttpClient.post<IRoomsRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms` , data)
   }
 
-  viewRoom(id : number |string):Observable<IRoomsRes>{
+
+  viewRoomDetails(id : number |string):Observable<IRoomsRes>{
     return this._HttpClient.get<IRoomsRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms${id}`)
   }
 
-  editRoom(id : number |string , data :any):Observable<IAddRoomRes>{
-    return this._HttpClient.put<IAddRoomRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms${id}` , data)
+  editRoom(id : number |string , data :any):Observable<IRoomsRes>{
+    return this._HttpClient.put<IRoomsRes>(`https://upskilling-egypt.com:3000/api/v0/admin/rooms${id}` , data)
   }
 
  deleteRoom(id : number |string):Observable<any>{
     return this._HttpClient.delete(`admin/rooms${id}`)
   }
-
 }
