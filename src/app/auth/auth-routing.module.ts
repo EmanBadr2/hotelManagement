1ae2b78c96f1cf721d37c67c17d28a67fa926c41
WalaaElqady <walaaatef194@gmail.com>
2025-04-21 01:56:14 +0200

    handle auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,14 +4,20 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { AuthLayoutComponent } from './components/auth-layout/auth-layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo:'login' , pathMatch:'full' } ,
-  { path: 'login', component: LoginComponent } ,
-  { path: 'register', component: RegisterComponent } ,
-  { path: 'forgetPassword', component: ForgetPasswordComponent } ,
-  { path: 'resetPassword', component: ResetPasswordComponent } ,
-
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgetPassword', component: ForgetPasswordComponent },
+      { path: 'resetPassword', component: ResetPasswordComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -11,26 +11,23 @@ export class AuthService {
 
   constructor(private _HttpClient:HttpClient) { }
   register(data: FormData): Observable<any> {
-    return this._HttpClient.post(
-      'https://upskilling-egypt.com:3000/api/v0/portal/users',
-      data
-    );
+    return this._HttpClient.post('portal/users', data);
   }
   login(data: any): Observable<any> {
-    return this._HttpClient.post('https://upskilling-egypt.com:3000/api/v0/admin/users/login', data);
+    return this._HttpClient.post('portal/users/login', data);
   }
 
 
   resetPassword(data: FormGroup): Observable<any> {
     return this._HttpClient.post(
-      'https://upskilling-egypt.com:3000/api/v0/portal/users/reset-password',
+      'portal/users/reset-password',
       data
     );
   }
 
   forgetPassword(data: any): Observable<any> {
     return this._HttpClient.post(
-      `https://upskilling-egypt.com:3000/api/v0/portal/users/forgot-password`,
+      `portal/users/forgot-password`,
       data
     );
   }
