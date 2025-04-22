52a09f1f54db6bd459f17e9c50f927cf71a9c4ff
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-04-23 01:13:08 +0200

    feat : create landing navbar style and template

--- src/app/landing/components/navbar/navbar.component.ts
+++ src/app/landing/components/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-landing-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  menuOpen = false;
+ 
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
