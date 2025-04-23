81136d2d1c3a676a5108e26a847688cf1a5c6064
emanbadr <amonaelshafey252@gmail.com>
2025-04-23 19:04:22 +0200

    handle layout

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -4,7 +4,7 @@ import { LandingComponent } from './components/landing/landing.component';
 import { CurdsComponent } from './components/curds/curds.component';
 
 const routes: Routes = [
-  { path: '', component: CurdsComponent },
+  { path: '', component: LandingComponent },
   { path: 'explore', loadChildren: () => import('./modules/explore/explore.module').then(m => m.ExploreModule) },
   { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) },
   { path: 'fav-items', loadChildren: () => import('./modules/fav-items/fav-items.module').then(m => m.FavItemsModule) },
