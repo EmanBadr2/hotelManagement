06a8b043c1a1dc52bc8603c296d7fde712b0f1c2
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-04-23 14:48:55 +0200

    chore : add isLoggedIn and Routerlinks

--- src/core/services/storage.service.ts
+++ src/core/services/storage.service.ts
@@ -1,40 +1,39 @@
 import { Injectable } from '@angular/core';
-import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
-  userRole :string | null = localStorage.getItem('userRole')
-  token :string | null = localStorage.getItem('token')
-  userID :string | null =  localStorage.getItem('userID')
-  userName :string| null =  localStorage.getItem('userName')
-  decodedToken !: any
-  isAdmin !: boolean
-  isUser!: boolean
-  isLogged !: boolean
+  userRole: string | null = localStorage.getItem('userRole');
+  token: string | null = localStorage.getItem('token');
+  userID: string | null = localStorage.getItem('userID');
+  userName: string | null = localStorage.getItem('userName');
+  decodedToken!: any;
+  isAdmin!: boolean;
+  isUser!: boolean;
+  isLogged!: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  loadUserRole(): void{   //call to check role user or admin
-    if(this.token){
-      this.decodedToken= jwtDecode(this.token);
+  loadUserRole(): void {
+    //call to check role user or admin
+    if (this.token) {
+      this.decodedToken = jwtDecode(this.token);
       this.userRole = this.decodedToken.role; // Extract role from token
       localStorage.setItem('userRole', this.userRole || ''); // Store role in localStorage
     }
-    if(this.userRole == 'admin'){
-      this.isAdmin = true
-    }
-    else{
-      this.isUser=true
-    }
-  }
-
-  isLoggedIn():void {  // call to check is loggedIn
-    if( this.token){
-      this.isLogged = true
+    if (this.userRole == 'admin') {
+      this.isAdmin = true;
+    } else {
+      this.isUser = true;
     }
   }
 
-
+  isLoggedIn(): void {
+    // call to check is loggedIn
+    if (this.token) {
+      this.isLogged = true;
+    }
+  }
 }
