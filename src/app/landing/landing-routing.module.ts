996274e758c31a8e1b021a5ad9cfda0820980548
emanbadr <amonaelshafey252@gmail.com>
2025-04-26 02:00:24 +0300

    handle routes

--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -4,7 +4,7 @@ import { LandingComponent } from './components/landing/landing.component';
 import { HappyComponent } from './components/happy/happy.component';
 
 const routes: Routes = [
-  { path: '', component: HappyComponent },
+  { path: '', component: LandingComponent },
   { path: 'explore', loadChildren: () => import('./modules/explore/explore.module').then(m => m.ExploreModule) },
   { path: 'booking', loadChildren: () => import('./modules/booking/booking.module').then(m => m.BookingModule) },
   { path: 'fav-items', loadChildren: () => import('./modules/fav-items/fav-items.module').then(m => m.FavItemsModule) },
