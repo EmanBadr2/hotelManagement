2fe28b96b5b80e8f7cc9f161a3e29a2ae8e1717f
Muhammad Bakhit <mohamedbakhetoiu@gmail.com>
2025-04-25 21:42:18 +0300

    uplode

--- src/app/landing/components/curds/curds.component.ts
+++ src/app/landing/components/curds/curds.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LandingService } from '../../services/landing.service';
 import { Curd } from '../../interface/curd';
-import { FACILITY_MAP } from '../../interface/facility-map';
 
 @Component({
   selector: 'app-curds',
@@ -10,7 +9,6 @@ import { FACILITY_MAP } from '../../interface/facility-map';
 })
 export class CurdsComponent implements OnInit {
   rooms: Curd[] = [];
-  facilityMap = FACILITY_MAP;
 constructor(private _LandingService:LandingService) { }
 
 ngOnInit() {
