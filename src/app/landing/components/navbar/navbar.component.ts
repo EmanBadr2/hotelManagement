b5194523efe2587b908ae6902672abf11a4636f7
Mohamed Osama <mohamed.mahgoub@gizasystems.com>
2025-04-30 17:56:08 +0300

    chore(Navbar&StorgeService) : update navbar and storgeService

--- src/app/landing/components/navbar/navbar.component.ts
+++ src/app/landing/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { StorageService } from 'src/core/services/storage.service';
 
@@ -9,9 +10,18 @@ import { StorageService } from 'src/core/services/storage.service';
 })
 export class NavbarComponent implements OnInit {
   selectedLang = 'en';
+  menuOpen = false;
+  userName = '';
+  isUser: boolean = false;
+  isUserLoggedIn: boolean = false; // Replace with actual user check
+  userId = localStorage.getItem('userID') || ''; // Get the user ID from local storage
 
-  constructor(private translate: TranslateService) {
-    const testUser = localStorage.getItem('userRole')
+  constructor(
+    private translate: TranslateService,
+    private route: Router,
+    private serviceStorage: StorageService
+  ) {
+    const testUser = localStorage.getItem('userRole');
     console.log(testUser);
 
     const lang = localStorage.getItem('lang') || 'en';
@@ -19,11 +29,21 @@ export class NavbarComponent implements OnInit {
     this.translate.setDefaultLang(lang);
     this.translate.use(lang);
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
-
-
-    // document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
   }
 
+  ngOnInit() {
+    this.serviceStorage.loadFromLocalStorage(); // Refresh service values
+    this.userName = this.serviceStorage.userName || '';
+    this.isUser = this.serviceStorage.isUser;
+  }
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  logout() {
+    this.isUser = false;
+    localStorage.clear();
+    this.route.navigate(['/auth/login']);
+  }
   changeLanguage(lang: string) {
     console.log('Selected lang:', lang);
     this.selectedLang = lang;
@@ -32,30 +52,4 @@ export class NavbarComponent implements OnInit {
     this.translate.use(lang);
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
   }
-  menuOpen = false;
-  userName = '';
-  isUser:boolean =false
-  isUserLoggedIn: boolean = false; // Replace with actual user check
-  serviceStorage = inject(StorageService);
-  userId = localStorage.getItem('userID') || ''; // Get the user ID from local storage
-  ngOnInit() {
-    this.userName = this.serviceStorage.userName || ''; // Get the user name from the service
-    if( this.serviceStorage.isUser){
-      this.isUser =true
-    }
-  }
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
-  logout(){
-    this.isUser =false
-    localStorage.clear()
-
-
-  }
-
 }
-
-
-
-
